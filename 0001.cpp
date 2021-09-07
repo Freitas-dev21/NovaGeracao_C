99e4ff13b5a27130a19a6a26e8336f2e01541cae
rafael <rafadev21@gmail.com>
2021-09-07 16:38:01 -0300

    estudos 3.1

--- 0001.cpp
+++ 0001.cpp
@@ -1,7 +1,15 @@
 #include <stdio.h> // para print()
 #include<stdlib.h> // para system()
 int main(){
-	printf("Primeiro programa 5.\n");
+	/*Variaveis devem ser declarada no inicio do bloco...*/
+	int num1;
+	int num2;
+	
+	num1 = 44;
+	num2 = num1 + 20;
+	
+	printf("\n O primeiro numero eh: %d",num1);
+	printf("\n O segundo numero eh : \" num1 + 20 \"= %d \n ",num2);
 	system("PAUSE");
 	return 0;
 }
